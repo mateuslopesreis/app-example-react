55ea8150b4f4fcc450aa911e8701cd12848000d2
mateuslopesreis <mateus.reis@estudante.ifms.edu.br>
2023-03-05 16:48:59 -0300

    adicionando o UseState no titulo e nas imagens

--- src/components/Gallery/data.ts
+++ src/components/Gallery/data.ts
@@ -8,7 +8,7 @@ export const movies = [
         "duration": "1h 47m",
         "parentalRating": "12",
         "images": {
-            "background": "https://www.themoviedb.org/t/p/w500_and_h282_face/1DBDwevWS8OhiT3wqqlW7KGPd6m.jpg",
+            "background": "https://www.themoviedbthemoviedb.org/t/p/w500_and_h282_face/1DBDwevWS8OhiT3wqqlW7KGPd6m.jpg",
             "poster": "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/53AC95HTGvFYA8ZRkGgeYo7lBRo.jpg"
         }
     },
--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,15 +1,48 @@
+import { useState } from 'react'
 import { movies } from './data'
+import './style.css'
 
 export function Gallery(){
-    let movie = movies[0]
+    console.log('renderizou')
+
+    /* let NovoMovie = movies[0] */
+    let aum = 0
+
+    const [movie,setMovie] = useState(movies[0])
+
+
+    function handleInput(){
+          setMovie(movies[length++])
+    }
+
+    function handleInput2(){
+        setMovie(movies[length--])
+  }
+
     return(
         <>
             <section className="gallery">
+            
             <img src={movie.images.poster}></img>
+            <p>Titulo: </p><h3>{movie.title}</h3>
+            <p>Duração: </p><h3>{movie.duration}</h3>
+            
+            
+            <button className='botoes' onClick={handleInput}>Anterior</button>
+            <button className='botoes'onClick={handleInput2}>Próxima</button>
             </section>
-            <button>Anterior</button>
-            <button>Próxima</button>
-           
+            
         </>
     )
 }
+
+/* 
+const [name,setName] = useState('')
+
+
+
+return(
+    <>
+        <h1>State Example</h1>
+        <input onInput={handleInput} className='inputName' type='text'/>
+        <h1>{name}</h1> */ 
