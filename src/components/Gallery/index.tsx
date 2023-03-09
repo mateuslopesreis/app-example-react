7c6ba574359d31f67c72d43a5ec5b9b7ef51825b
mateuslopesreis <mateus.reis@estudante.ifms.edu.br>
2023-03-08 23:38:50 -0300

    melhorando o useState

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -6,18 +6,27 @@ export function Gallery(){
     console.log('renderizou')
 
     /* let NovoMovie = movies[0] */
-    let aum = 0
+    
 
-    const [movie,setMovie] = useState(movies[0])
-
+    const [listar,setListar] = useState(0)
+    let movie = movies[listar]
 
     function handleInput(){
-          setMovie(movies[length++])
+            
+          
+            console.log(movies.length)
+          /* if(movies.length>15){ */
+            console.log('deu')
+          
+            setListar(listar+1);
+          
     }
 
     function handleInput2(){
-        setMovie(movies[length--])
+        setListar(listar-1)
   }
+
+  
 
     return(
         <>
@@ -36,13 +45,40 @@ export function Gallery(){
     )
 }
 
-/* 
-const [name,setName] = useState('')
 
 
 
-return(
-    <>
-        <h1>State Example</h1>
-        <input onInput={handleInput} className='inputName' type='text'/>
-        <h1>{name}</h1> */ 
+    /*
+    
+    -------------------------------------------------------------------------------------------
+    Primeiro teste de useState
+
+
+    const [movie,setMovie] = useState(movies[0])
+
+
+    function handleInput(){
+          setMovie(movies[length++])
+    }
+
+    function handleInput2(){
+        setMovie(movies[length--])
+  }
+
+    
+    
+    return(
+        <>
+            <section className="gallery">
+            
+            <img src={movie.images.poster}></img>
+            <p>Titulo: </p><h3>{movie.title}</h3>
+            <p>Duração: </p><h3>{movie.duration}</h3>
+            
+            
+            <button className='botoes' onClick={handleInput}>Anterior</button>
+            <button className='botoes'onClick={handleInput2}>Próxima</button>
+            </section>
+            
+        </> */
+
